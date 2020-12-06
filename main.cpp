6f3e4748fbe1b6383aefa5a43647a617e20975d5
Ally Delgado <61644740+aedelgado19@users.noreply.github.com>
2020-12-05 17:23:04 -0800

    fixes to stack functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
  */
 
 #include <iostream>
+<<<<<<< HEAD
+#include <cstring>
+=======
+>>>>>>> 9280c3826c56a0d53fa681a923db7745b6b94c18
 #include "BinaryTree.h"
 using namespace std;
 
@@ -12,15 +16,92 @@ struct Node{
 };
 
 // stack and queue function prototypes **********
+<<<<<<< HEAD
+void push(Node* &headOfStack, char* inData);
+char* pop(Node* &headOfStack);
+char* peek(Node* &headOfStack);
+void enqueue(Node* headOfQueue, char* inData);
+=======
 void push(Node* &headOfStack, char* data);
 void pop(Node* &headOfStack);
 char* peek(Node* &headOfStack);
 void display();
 void enqueue();
+>>>>>>> 9280c3826c56a0d53fa681a923db7745b6b94c18
 void dequeue();
 // helper functions ****************************
 bool isEmpty(Node* &head);
 // *********************************************
+<<<<<<< HEAD
+
+//adds a new node after the last one and moves "last" to next
+void enqueue(Node* &headOfQueue, char* inData){
+    
+}
+
+//removes top node and moves next node to "top"
+void dequeue(Node* &headOfQueue){
+    
+}
+
+//takes in data, creates a new node, and stacks it on top of stack LL
+void push(Node* &headOfStack, char* inData){
+  Node* newnode = NULL;
+  if(headOfStack == NULL){
+    headOfStack = newnode;
+    newnode->data = inData;
+    newnode->next = NULL;
+  } else {
+    newnode->next = headOfStack;
+    newnode->data = inData;
+    headOfStack = newnode;
+  }
+}
+
+//used in peek to see if empty
+bool isEmpty(Node *&head){
+  if(head == NULL){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+//returns top element
+char* peek(Node *&headOfStack){
+  bool empty = isEmpty(headOfStack);
+  if(empty == false){ 
+    cout << "Peek: not an empty stack!" << endl;
+    return headOfStack->data;
+  } else {
+    cout << "Peek: Empty stack. " << endl;
+  }
+  cout << "Peek: returning null." << endl;
+  return NULL;
+}
+
+//delete head of stack
+char* pop(Node* &headOfStack){
+  Node* temp = new Node;
+
+  if(headOfStack != NULL){
+    int dataSize = strlen(headOfStack->data);
+    char* backupData = new char[dataSize];
+    temp = headOfStack; //original head
+    headOfStack = headOfStack->next; //new "head"
+    temp->next = NULL; //disconnect original from next
+    strcpy(backupData, temp->data);
+    delete(temp);
+    cout << "Pop: deleted head" << endl;
+    return backupData;
+  } else {
+    cout << "Pop: head of stack is null" << endl;
+  }
+  return NULL;
+}
+
+=======
 
 //takes in data, creates a new node, and stacks it on top of stack LL
 void push(Node* &headOfStack, char* data){
@@ -72,6 +153,7 @@ void pop(Node* &headOfStack){
   }
 }
 
+>>>>>>> 9280c3826c56a0d53fa681a923db7745b6b94c18
 int main(){
   Node* headOfStack = NULL;
   Node* headOfQueue = NULL;
